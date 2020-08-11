a4297b1d9a779070e66670ba0f366d99f110c7ca
Rnazx <42196798+Rnazx@users.noreply.github.com>
2020-08-11 15:19:11 +0530

    Update and rename Q3.py to Q3_revised.py

--- Q3_revised.py
+++ Q3_revised.py
@@ -9,6 +9,5 @@ p = float(input("Enter the desired accuracy "))
 i=1
 while hsum(i+1)-hsum(i)>p:
     i=i+1
-print("{0},{1},{2},{3},{4}".format(hsum(998),hsum(999),hsum(1000),hsum(1001),hsum(1002)))
 print("The sum upto {0} accuracy is {1}".format(p,hsum(i+1)))
 print("The number of terms we need to add is",i+1)
