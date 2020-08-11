1b9c3e461406aaf566505b5a3f9984642f0ea3cb
Rnazx <42196798+Rnazx@users.noreply.github.com>
2020-08-11 15:17:43 +0530

    Add files via upload

--- Q3.cpp
+++ Q3.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
 using namespace std;
+float hsum(int n){
+    float sum=0;
+    for(float i=1;i<=n;i++){
+        sum=sum+1/i;
+    }
+    return sum;
+}
 int main(){
-    double c=0;
-    double i=1;
+    int i=1;
     double p;
     cout<<"Enter the desired accuracy: ";
     cin>>p;
-    while(1/i>p){
-        double k=1/i;
-        c=c+k;
+    while(hsum(i+1)-hsum(i)>p){
         i++;
     }
-    cout<<"The sum upto "<<p<<" accuracy is "<<c<<endl;
-    cout<<"The number of terms we need to add is "<<i<<endl;
+    cout<<"The sum upto "<<p<<" accuracy is "<<hsum(i+1)<<endl;
+    cout<<"The number of terms we need to add is "<<i+1<<endl;
     
 }
--- Q3.py
+++ Q3.py
@@ -1,8 +1,14 @@
+def hsum(n):
+    sum=0
+    i=1
+    while i<=n:
+        sum+=1/i
+        i=i+1
+    return sum
 p = float(input("Enter the desired accuracy "))
 i=1
-sum=0
-while 1/i>p:
-    sum=sum+1/i
+while hsum(i+1)-hsum(i)>p:
     i=i+1
-print("The sum upto {0} accuracy is {1}".format(p,sum))
-print("The number of terms we need to add is",i)
+print("{0},{1},{2},{3},{4}".format(hsum(998),hsum(999),hsum(1000),hsum(1001),hsum(1002)))
+print("The sum upto {0} accuracy is {1}".format(p,hsum(i+1)))
+print("The number of terms we need to add is",i+1)
